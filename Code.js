1b049ae1ca4c83aa9364bd24db0673ed1ba330e7
tanyaMudita <76211665+tanyaMudita@users.noreply.github.com>
2021-05-04 18:15:15 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+ // add code for displaying rest of the names and the hints.
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint:a set of steps that result in a single, specific action !!", 700,170);
+    
+    fill("white")
+    textSize(15)
+    text("E T A D A S B A ", 100,270)
+    fill("lightblue")
+    text("Hint: Information stored in multiplayer game !!", 100,290)
+   
+
+}
